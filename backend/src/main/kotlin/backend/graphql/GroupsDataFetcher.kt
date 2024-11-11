2566bd304dba1deba97f6ee00787ebf96af3d6ea
Szymon Nowak-Trzos <40152296+Szyntos@users.noreply.github.com>
2024-11-12 00:02:19 +0100

    Added automatic photo assign for groups (#158)

--- backend/src/main/kotlin/backend/graphql/GroupsDataFetcher.kt
+++ backend/src/main/kotlin/backend/graphql/GroupsDataFetcher.kt
@@ -240,6 +240,10 @@ class GroupsDataFetcher {
             edition = edition
         )
         groupsRepository.save(group)
+        val filesFromGroupInEdition = groupsRepository.findByEdition(edition).mapNotNull { it.imageFile }
+        val file = fileRepository.findAllByFileType("image/group").filter { it !in filesFromGroupInEdition }.shuffled().firstOrNull()
+        val fileId = file?.fileId ?: fileRepository.findAllByFileType("image/group").shuffled().firstOrNull()?.fileId
+        photoAssigner.assignPhotoToAssignee(groupsRepository, "image/group", group.groupsId, fileId)
         val userGroups = UserGroups(
             user = teacher,
             group = group
@@ -313,7 +317,12 @@ class GroupsDataFetcher {
             edition = edition
         )
         groupsRepository.save(group)
-        photoAssigner.assignPhotoToAssignee(groupsRepository, "image/group", group.groupsId, null)
+
+        val filesFromGroupInEdition = groupsRepository.findByEdition(edition).mapNotNull { it.imageFile }
+        val file = fileRepository.findAllByFileType("image/group").filter { it !in filesFromGroupInEdition }.shuffled().firstOrNull()
+        val fileId = file?.fileId ?: fileRepository.findAllByFileType("image/group").shuffled().firstOrNull()?.fileId
+        photoAssigner.assignPhotoToAssignee(groupsRepository, "image/group", group.groupsId, fileId)
+
         val userGroups = UserGroups(
             user = teacher,
             group = group

e3dc89d41d7ea3d6fb8afaede841b81a229cd89d
AnCichocka <annacichocka.poczta@gmail.com>
2024-11-14 21:56:24 +0100

    add and edit grading checks

---
+++ frontend/src/components/Edition/Sections/GradingChecksSection/ChecksForm.tsx
@@ -0,0 +1,172 @@
+import { z, ZodError } from "zod";
+import { FormikErrors, useFormik } from "formik";
+import {
+  TextField,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select,
+} from "@mui/material";
+import { Styles } from "../../../../utils/Styles";
+import { Category } from "../../../../hooks/Edition/categories/useCategoriesSection";
+import { LevelSet } from "../../../../hooks/Edition/useLevelSetsSection";
+
+const ValidationSchema = z.object({
+  endOfLabsDate: z.string().min(1), // regexp
+  endOfLabsLevelsThreshold: z.string().min(1),
+  projectPointsThreshold: z.string().min(1),
+  projectId: z.string().min(1),
+});
+
+export type GradingChecksFormValues = z.infer<typeof ValidationSchema>;
+
+type GradingChecksFormProps = {
+  handleConfirm: (values: GradingChecksFormValues) => void;
+  formError?: string;
+  levelSet: LevelSet;
+  initialValues?: GradingChecksFormValues;
+  title: string;
+  categories: Category[];
+};
+
+const defaultInitialValues: GradingChecksFormValues = {
+  endOfLabsDate: "",
+  endOfLabsLevelsThreshold: "",
+  projectPointsThreshold: "",
+  projectId: "",
+};
+
+export const ChecksForm = ({
+  handleConfirm,
+  levelSet,
+  formError,
+  initialValues = defaultInitialValues,
+  title,
+  categories,
+}: GradingChecksFormProps) => {
+  const formik = useFormik({
+    initialValues,
+    validate: (values: GradingChecksFormValues) => {
+      const errors: FormikErrors<GradingChecksFormValues> = {};
+      try {
+        ValidationSchema.parse(values);
+      } catch (error) {
+        if (error instanceof ZodError) {
+          Object.assign(errors, error.formErrors.fieldErrors);
+        }
+      }
+      return errors;
+    },
+    onSubmit: (values: GradingChecksFormValues) => {
+      handleConfirm({ ...values });
+    },
+  });
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.title}>{title}</div>
+      <form onSubmit={formik.handleSubmit}>
+        <div style={styles.fieldsContainer}>
+          <TextField
+            fullWidth
+            name="endOfLabsDate"
+            label="endOfLabsDate"
+            variant="outlined"
+            value={formik.values.endOfLabsDate}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={Boolean(
+              formik.touched.endOfLabsDate && formik.errors.endOfLabsDate,
+            )}
+            helperText={
+              formik.touched.endOfLabsDate && formik.errors.endOfLabsDate
+            }
+          />
+          <FormControl fullWidth>
+            <InputLabel>End of Labs</InputLabel>
+            <Select
+              name="endOfLabsLevelsThreshold"
+              value={formik.values.endOfLabsLevelsThreshold}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={Boolean(
+                formik.touched.endOfLabsLevelsThreshold &&
+                  formik.errors.endOfLabsLevelsThreshold,
+              )}
+            >
+              {levelSet.levels.map((level) => (
+                <MenuItem key={level.name} value={level.levelId}>
+                  {level.name}
+                </MenuItem>
+              ))}
+            </Select>
+            {formik.touched.projectId && formik.errors.projectId && (
+              <div style={{ color: "red" }}>{formik.errors.projectId}</div>
+            )}
+          </FormControl>
+          <TextField
+            fullWidth
+            name="projectPointsThreshold"
+            label="projectPointsThreshold"
+            type="number"
+            variant="outlined"
+            value={formik.values.projectPointsThreshold}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={Boolean(
+              formik.touched.projectPointsThreshold &&
+                formik.errors.projectPointsThreshold,
+            )}
+            helperText={
+              formik.touched.projectPointsThreshold &&
+              formik.errors.projectPointsThreshold
+            }
+          />
+          <FormControl fullWidth>
+            <InputLabel>Project</InputLabel>
+            <Select
+              name="projectId"
+              value={formik.values.projectId}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={Boolean(
+                formik.touched.projectId && formik.errors.projectId,
+              )}
+            >
+              {categories.map((category) => (
+                <MenuItem
+                  key={category.categoryName}
+                  value={category.categoryId}
+                >
+                  {category.categoryName}
+                </MenuItem>
+              ))}
+            </Select>
+            {formik.touched.projectId && formik.errors.projectId && (
+              <div style={{ color: "red" }}>{formik.errors.projectId}</div>
+            )}
+          </FormControl>
+        </div>
+        <button type="submit">confirm</button>
+      </form>
+      {formError && <p style={styles.error}>Error: {formError}</p>}
+    </div>
+  );
+};
+
+const styles: Styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 12,
+    padding: 12,
+    width: 500,
+  },
+  title: { fontWeight: "bold" },
+  error: { color: "red" },
+  fieldsContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 12,
+  },
+};
--- frontend/src/components/Edition/Sections/GradingChecksSection/GradingChecksSection.tsx
+++ frontend/src/components/Edition/Sections/GradingChecksSection/GradingChecksSection.tsx
@@ -1,14 +1,95 @@
+import { Dialog } from "@mui/material";
+import { useGradingChecksSection } from "../../../../hooks/Edition/useGradingChecksSection";
 import { Styles } from "../../../../utils/Styles";
 
 import { useParams } from "react-router-dom";
+import { CloseHeader } from "../../../dialogs/CloseHeader";
+import { ChecksForm } from "./ChecksForm";
+import { EMPTY_FIELD_STRING } from "../../../../utils/constants";
 
 export const GradingChecksSection = () => {
   const params = useParams();
   const editionId = params.id ? parseInt(params.id) : -1;
 
+  const {
+    gradingChecks,
+    formCategories,
+    formLevelSet,
+    loading,
+    error,
+
+    formError,
+
+    isAddOpened,
+    closeAdd,
+    openAdd,
+    handleAdd,
+
+    isEditOpened,
+    openEdit,
+    closeEdit,
+    handleEdit,
+  } = useGradingChecksSection(editionId);
+
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>ERROR: {error.message}</div>;
+
   return (
     <div style={styles.container}>
       <div>grading checks: {editionId}</div>
+
+      <button onClick={gradingChecks ? openEdit : openAdd}>
+        {gradingChecks ? "edit checks" : "add checks"}
+      </button>
+
+      <div>
+        <div>
+          endOfLabsDate: {gradingChecks?.endOfLabsDate ?? EMPTY_FIELD_STRING}
+        </div>
+        <div>
+          endOfLabsLevelsThreshold:{" "}
+          {gradingChecks?.endOfLabsLevelsThreshold ?? EMPTY_FIELD_STRING}
+        </div>
+        <div>projectId: {gradingChecks?.projectId ?? EMPTY_FIELD_STRING}</div>
+        <div>
+          projectPointsThreshold:{" "}
+          {gradingChecks?.projectPointsThreshold ?? EMPTY_FIELD_STRING}
+        </div>
+      </div>
+
+      <Dialog open={isAddOpened}>
+        <CloseHeader onCloseClick={closeAdd} />
+        <ChecksForm
+          formError={formError}
+          handleConfirm={handleAdd}
+          categories={formCategories}
+          title="Add Grading Checks"
+          levelSet={formLevelSet}
+        />
+      </Dialog>
+
+      <Dialog open={isEditOpened}>
+        <CloseHeader onCloseClick={closeEdit} />
+        <ChecksForm
+          formError={formError}
+          handleConfirm={handleEdit}
+          categories={formCategories}
+          levelSet={formLevelSet}
+          initialValues={
+            gradingChecks
+              ? {
+                  endOfLabsDate: gradingChecks.endOfLabsDate,
+                  endOfLabsLevelsThreshold:
+                    gradingChecks.endOfLabsLevelsThreshold,
+                  projectPointsThreshold:
+                    gradingChecks.projectPointsThreshold.toString(),
+                  projectId: gradingChecks.projectId,
+                }
+              : undefined
+          }
+          title="Edit Grading Checks"
+        />
+      </Dialog>
     </div>
   );
 };

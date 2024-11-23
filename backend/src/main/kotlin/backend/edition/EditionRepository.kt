2cc4b2b12f5b19c5fcfef05a3fe67cdaf3358f0e
Szymon Nowak-Trzos <40152296+Szyntos@users.noreply.github.com>
2024-11-23 18:12:00 +0100

    Added editions to getCurrentUser (#179)

--- backend/src/main/kotlin/backend/edition/EditionRepository.kt
+++ backend/src/main/kotlin/backend/edition/EditionRepository.kt
@@ -1,5 +1,6 @@
 package backend.edition
 
+import backend.users.Users
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -10,4 +11,5 @@ interface EditionRepository : JpaRepository<Edition, Long> {
     fun existsByEditionYear(year: Int): Boolean
     fun existsByEditionName(name: String): Boolean
     fun findAllByEditionName(name: String): List<Edition>
+    fun findAllByGroups_UserGroups_User(user: Users): List<Edition>
 }

fda07ea478d03e9530609b680c9b63594043801c
Szymon Nowak-Trzos <szymtos@gmail.com>
2024-12-09 00:28:54 +0100

    Fix

--- backend/src/main/python/init_data.py
+++ backend/src/main/python/init_data.py
@@ -1,43 +1,23 @@
-import psycopg2
 import random
 import json
-from utils.insert_files import insert_files
+from utils.insert_init_files import insert_init_files
 from utils.insert_initial_user import insert_initial_coordinator
 
 # Load configuration from config.json
-with open('config.json', encoding="UTF-8") as config_file:
+with open('config_init.json', encoding="UTF-8") as config_file:
     config = json.load(config_file)
 
 # Extract values from the configuration
-db_config = config['database']
 base_url = config['base_url']
 hasura_url = config['hasura']['url']
 headers = config['hasura']['headers']
 data_insertion_config = config['data_insertion']
-backend_resources_path = data_insertion_config['backend_resources_path']
-
-
-def create_connection():
-    return psycopg2.connect(
-        dbname=db_config['dbname'],
-        user=db_config['user'],
-        password=db_config['password'],
-        host=db_config['host'],
-        port=db_config['port']
-    )
 
 def insert_data():
-    insert_files(base_url + "/files/upload")
+    insert_init_files(base_url + "/files/upload")
     coordinator_id_and_role = insert_initial_coordinator(hasura_url, headers)
 
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
 if __name__ == '__main__':
-    conn = create_connection()
-    cursor = conn.cursor()
 
     seed = data_insertion_config['seed']
     random.seed(seed)
--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -1,14 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+const getEnvVariable = (key: string, fallback: string = ""): string => {
+  const value = (import.meta.env[key] as string | undefined) ?? fallback;
+  if (!value) {
+    console.warn(`Environment variable ${key} is not defined.`);
+  }
+  return value;
+};
+
+export const firebaseConfig = {
+  apiKey: getEnvVariable("VITE_FIREBASE_API_KEY"),
+  authDomain: getEnvVariable("VITE_FIREBASE_AUTH_DOMAIN"),
+  projectId: getEnvVariable("VITE_FIREBASE_PROJECT_ID"),
+  storageBucket: getEnvVariable("VITE_FIREBASE_STORAGE_BUCKET"),
+  messagingSenderId: getEnvVariable("VITE_FIREBASE_MESSAGING_SENDER_ID"),
+  appId: getEnvVariable("VITE_FIREBASE_APP_ID"),
+  measurementId: getEnvVariable("VITE_FIREBASE_MEASUREMENT_ID"),
 };
 
 // Initialize Firebase

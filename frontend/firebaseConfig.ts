c056a8500055707fcf0a059d8b0086f97c9b46c5
Szymon Nowak-Trzos <40152296+Szyntos@users.noreply.github.com>
2024-12-10 20:53:59 +0100

    [0] Ready for realease (#203)
    * Added {
    
    * docker-compose change
    
    * Centralized dotenvs
    
    * Added secrets-free firebaseConfig.ts
    
    * added export of env vars
    
    * moved devcontainer
    
    * Fox
    
    * Fox
    
    * Fix
    
    * fuix
    
    * fuix
    
    * fuix
    
    * Fix
    
    * Fix
    
    * Fixes
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * amd
    
    * Fix
    
    * arm
    
    * amd
    
    * amd
    
    * remove platform
    
    * both on amd
    
    * Fixed posturl
    
    * publicIP
    
    * publicIP
    
    * publicIP
    
    * publicIP
    
    * fix
    
    * Fix
    
    * DO_INSERT_FILES
    
    * backend fix
    
    * Fixed local

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getEnvVariable } from "./src/utils/constants";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+export const firebaseConfig = {
+  apiKey: getEnvVariable("VITE_FIREBASE_API_KEY"),
+  authDomain: getEnvVariable("VITE_FIREBASE_AUTH_DOMAIN"),
+  projectId: getEnvVariable("VITE_FIREBASE_PROJECT_ID"),
+  storageBucket: getEnvVariable("VITE_FIREBASE_STORAGE_BUCKET"),
+  messagingSenderId: getEnvVariable("VITE_FIREBASE_MESSAGING_SENDER_ID"),
+  appId: getEnvVariable("VITE_FIREBASE_APP_ID"),
+  measurementId: getEnvVariable("VITE_FIREBASE_MEASUREMENT_ID"),
 };
 
 // Initialize Firebase
